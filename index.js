1ecc93d022f067f21a4dcd245262d54e541093cd
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-04-07 23:45:02 +0530

    main

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let entry=document.getElementById("text");
+let count=0;
+let counter=document.getElementById("count");
+function incre(){
+    count+=1;
+    counter.innerText=count; 
+}
+function save(){
+    let countstr=count+" - ";
+    entry.textContent += countstr;
+    counter.textContent=0;
+    count=0;
+    console.log(count);
+}
+
